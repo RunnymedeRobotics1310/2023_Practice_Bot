faa8a35fdb0385a5c3c1944604eab05b14f8f209
KaelinDavidson <kaelin.davidon@student.tdsb.on.ca>
2023-01-12 18:01:05 -0500

    I made a useless method to convert inches to cm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,4 +48,9 @@ public final class Constants {
         public static final int DRIVER_CONTROLLER_PORT = 0;
     }
 
+    public static int MetricConversion (x) { // Converts from inches to cm
+    	x = x * 2.54;
+    	return x;
+    }
+    
 }
