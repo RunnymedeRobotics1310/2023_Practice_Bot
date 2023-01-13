07499db17285c7d6a7b3d212e71c3673c5b6c02d
Richard McMullin <richard.mcmullin@altairix.com>
2023-01-13 11:06:23 -0500

    Add gyro routines to the DriveSubsystem.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,34 +16,40 @@ package frc.robot;
  */
 public final class Constants {
 
-	public static final class DriveConstants {
+    public static final class AutoConstants {
+	public static final String AUTO_PATTERN_DO_NOTHING = "Do nothing";
+	public static final String AUTO_PATTERN_MOVE = "Move";
+    }
 
-		public static final int LEFT_MOTOR_PORT = 10;
-		public static final int RIGHT_MOTOR_PORT = 20;
-		public static final boolean LEFT_MOTOR_REVERSED = false;
-		public static final boolean RIGHT_MOTOR_REVERSED = true;
+    public static final class DriveConstants {
 
-		public static final boolean LEFT_ENCODER_REVERSED = false;
-		public static final boolean RIGHT_ENCODER_REVERSED = true;
+	public static final int     LEFT_MOTOR_PORT = 10;
+	public static final int     RIGHT_MOTOR_PORT = 20;
 
-		public static final int ENCODER_COUNTS_PER_REVOLUTION = 1024;
-		public static final double ROBOT_WHEEL_DIAMETER_INCHES = 6;
+	public static final boolean LEFT_MOTOR_REVERSED = false;
+	public static final boolean RIGHT_MOTOR_REVERSED = true;
 
-		public static final double INCHES_PER_ENCODER_COUNT =
-		    // Assumes the encoders are directly mounted on the wheel shafts
-		    (ROBOT_WHEEL_DIAMETER_INCHES * Math.PI) / ENCODER_COUNTS_PER_REVOLUTION;
+	public static final boolean LEFT_ENCODER_REVERSED = false;
+	public static final boolean RIGHT_ENCODER_REVERSED = true;
 
-		public static final boolean GYRO_REVERSED = false;
-	}
+	public static final int ENCODER_COUNTS_PER_REVOLUTION = 1024;
+	public static final double ROBOT_WHEEL_DIAMETER_INCHES = 6;
 
-	public static final class AutoConstants {
-		public static final String AUTO_PATTERN_DO_NOTHING = "Do nothing";
-		public static final String AUTO_PATTERN_MOVE = "Move";
-	}
+	public static final double INCHES_PER_ENCODER_COUNT =
+		// Assumes the encoders are directly mounted on the wheel shafts
+		(ROBOT_WHEEL_DIAMETER_INCHES * Math.PI) / ENCODER_COUNTS_PER_REVOLUTION;
 
-	public static final class OiConstants {
+	public static final String GYRO_TYPE_NAVX     = "NAV_X";
+	public static final String GYRO_TYPE_ADXRS450 = "ADXRS_450";
+	public static final String GYRO_TYPE          = GYRO_TYPE_NAVX;
 
-		public static final int DRIVER_CONTROLLER_PORT = 0;
-	}
+	public static final boolean GYRO_REVERSED = false;
+
+    }
+
+    public static final class OiConstants {
+
+	public static final int DRIVER_CONTROLLER_PORT = 0;
+    }
 
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -3,10 +3,13 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
@@ -18,49 +21,156 @@ public class DriveSubsystem extends SubsystemBase {
     // The motors on the right side of the drive.
     private final TalonSRX rightPrimaryMotor  = new TalonSRX(DriveConstants.RIGHT_MOTOR_PORT);
     private final TalonSRX rightFollowerMotor = new TalonSRX(DriveConstants.RIGHT_MOTOR_PORT + 1);
-    private final AnalogInput distanceSensor = new AnalogInput(0);
+    private final AnalogInput distanceSensor  = new AnalogInput(0);
 
     private double leftSpeed = 0;
     private double rightSpeed = 0;
 
+    private ADXRS450_Gyro adxrs450Gyro = null;
+    private AHRS          navXGyro     = null;
+
+    private double gyroHeadingOffset = 0;
+
+    private enum GyroAxis {YAW, PITCH, ROLL};
+
     /** Creates a new DriveSubsystem. */
     public DriveSubsystem() {
 
-        // We need to invert one side of the drivetrain so that positive voltages
-        // result in both sides moving forward. Depending on how your robot's
-        // gearbox is constructed, you might have to invert the left side instead.
-        leftPrimaryMotor.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
-        leftFollowerMotor.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
+	// We need to invert one side of the drivetrain so that positive voltages
+	// result in both sides moving forward. Depending on how your robot's
+	// gearbox is constructed, you might have to invert the left side instead.
+	leftPrimaryMotor.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
+	leftFollowerMotor.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
 
-        leftPrimaryMotor.setNeutralMode(NeutralMode.Brake);
-        leftFollowerMotor.setNeutralMode(NeutralMode.Brake);
+	leftPrimaryMotor.setNeutralMode(NeutralMode.Brake);
+	leftFollowerMotor.setNeutralMode(NeutralMode.Brake);
 
-        leftFollowerMotor.follow(leftPrimaryMotor);
+	leftFollowerMotor.follow(leftPrimaryMotor);
 
 
-        rightPrimaryMotor.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
-        rightFollowerMotor.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
+	rightPrimaryMotor.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
+	rightFollowerMotor.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
 
-        rightPrimaryMotor.setNeutralMode(NeutralMode.Brake);
-        rightFollowerMotor.setNeutralMode(NeutralMode.Brake);
+	rightPrimaryMotor.setNeutralMode(NeutralMode.Brake);
+	rightFollowerMotor.setNeutralMode(NeutralMode.Brake);
 
-        rightFollowerMotor.follow(rightPrimaryMotor);
+	rightFollowerMotor.follow(rightPrimaryMotor);
 
-        // Setting both encoders to 0
-        resetEncoders();
+	// Setting both encoders to 0
+	resetEncoders();
+
+	if (Constants.DriveConstants.GYRO_TYPE == Constants.DriveConstants.GYRO_TYPE_NAVX) {
+	    navXGyro = new AHRS();
+	}
+	else {
+	    adxrs450Gyro = new ADXRS450_Gyro();
+	}
     }
 
-    public double getDistanceInches() {
-        return getAverageEncoderDistance() * DriveConstants.INCHES_PER_ENCODER_COUNT;
+    /**
+     * Calibrate Gyro
+     * <p>
+     * This routine calibrates the gyro.  The robot must not be moved during the
+     * calibrate routine which lasts about 10 seconds
+     */
+    public void calibrateGyro() {
+
+	gyroHeadingOffset = 0;
+
+	if (Constants.DriveConstants.GYRO_TYPE == Constants.DriveConstants.GYRO_TYPE_NAVX) {
+	    navXGyro.calibrate();
+	}
+	else {
+	    adxrs450Gyro.calibrate();
+	}
     }
 
+    /**
+     * Reset Gyro
+     * <p>
+     * This routine resets the gyro angle to zero.
+     * <p>
+     * NOTE: This is not the same as calibrating the gyro.
+     */
+    public void resetGyroHeading() {
+	setGyroHeading(0);
+    }
+
+    /**
+     * Set Gyro Heading
+     * <p>
+     * This routine sets the gyro heading to a known value.
+     */
+    public void setGyroHeading(double heading) {
+
+	// Clear the current offset.
+	gyroHeadingOffset = 0;
+
+	// Adjust the offset so that the heading is now the current heading.
+	gyroHeadingOffset = getHeading() - heading;
+    }
+
+    /**
+     * Gets the heading of the robot.
+     *
+     * @return heading in the range of 0 - 360 degrees
+     */
+    public double getHeading() {
+
+	double gyroAngle = getRawGyroAngle(GyroAxis.YAW);
+
+	// subtract the offset angle that was saved when the gyro
+	// was last rest.
+	gyroAngle -= gyroHeadingOffset;
+
+	// The angle can be positive or negative and extends beyond 360 degrees.
+	double heading = gyroAngle % 360.0;
+
+	if (heading < 0) {
+	    heading += 360;
+	}
+
+	return heading;
+    }
+
+    private double getRawGyroAngle(GyroAxis gyroAxis) {
+
+	if (Constants.DriveConstants.GYRO_TYPE == Constants.DriveConstants.GYRO_TYPE_NAVX) {
+	    switch (gyroAxis) {
+	    case YAW:
+		return navXGyro.getAngle();
+	    case PITCH:
+		return navXGyro.getPitch();
+	    case ROLL:
+		return navXGyro.getRoll();
+	    default:
+		return 0;
+	    }
+	}
+	else {
+	    switch (gyroAxis) {
+	    case YAW:
+		return adxrs450Gyro.getAngle();
+	    case PITCH:
+		return 0;
+	    case ROLL:
+		return 0;
+	    default:
+		return 0;
+	    }
+	}
+    }
     /**
      * Gets the average distance of the two encoders.
      *
      * @return the average of the two encoder readings
      */
     public double getAverageEncoderDistance() {
-        return (getLeftEncoder() + getRightEncoder()) / 2;
+	return (getLeftEncoder() + getRightEncoder()) / 2;
+    }
+
+    public double getDistanceInches() {
+	return getAverageEncoderDistance() * DriveConstants.INCHES_PER_ENCODER_COUNT;
     }
 
     /**
@@ -69,7 +179,7 @@ public class DriveSubsystem extends SubsystemBase {
      * @return the left drive encoder
      */
     public double getLeftEncoder() {
-        return 0; //leftEncoder.getPosition();
+	return 0; //leftEncoder.getPosition();
     }
 
     /**
@@ -78,14 +188,15 @@ public class DriveSubsystem extends SubsystemBase {
      * @return the right drive encoder
      */
     public double getRightEncoder() {
-        return 0; // rightEncoder.getPosition();
+	return 0; // rightEncoder.getPosition();
     }
 
     /** Resets the drive encoders to currently read a position of 0. */
     public void resetEncoders() {
-        //        rightEncoder.setPosition(0);
-        //        leftEncoder.setPosition(0);
+	//        rightEncoder.setPosition(0);
+	//        leftEncoder.setPosition(0);
     }
+
 
     /**
      * Set the left and right speed of the primary and follower motors
@@ -95,27 +206,35 @@ public class DriveSubsystem extends SubsystemBase {
      */
     public void setMotorSpeeds(double leftSpeed, double rightSpeed) {
 
-        this.leftSpeed = leftSpeed;
-        this.rightSpeed = rightSpeed;
+	this.leftSpeed = leftSpeed;
+	this.rightSpeed = rightSpeed;
 
-        leftPrimaryMotor.set(ControlMode.PercentOutput, leftSpeed);
-        rightPrimaryMotor.set(ControlMode.PercentOutput, rightSpeed);
+	leftPrimaryMotor.set(ControlMode.PercentOutput, leftSpeed);
+	rightPrimaryMotor.set(ControlMode.PercentOutput, rightSpeed);
 
-        // NOTE: The follower motors are set to follow the primary
-        // motors
+	// NOTE: The follower motors are set to follow the primary
+	// motors
     }
 
     @Override
     public void periodic() {
 
-        SmartDashboard.putNumber("Right Motor", rightSpeed);
-        SmartDashboard.putNumber("Left  Motor", leftSpeed);
+	SmartDashboard.putNumber("Right Motor", rightSpeed);
+	SmartDashboard.putNumber("Left  Motor", leftSpeed);
 
-        SmartDashboard.putNumber("Right Encoder", getRightEncoder());
-        SmartDashboard.putNumber("Left Encoder", getLeftEncoder());
+	SmartDashboard.putNumber("Right Encoder", getRightEncoder());
+	SmartDashboard.putNumber("Left Encoder", getLeftEncoder());
 
-        SmartDashboard.putNumber("Distance (inches)", getDistanceInches());
+	SmartDashboard.putNumber("Distance (inches)", getDistanceInches());
 
-        SmartDashboard.putNumber("AnalogInput", distanceSensor.getVoltage());
+	SmartDashboard.putNumber("AnalogInput", distanceSensor.getVoltage());
+
+	if (Constants.DriveConstants.GYRO_TYPE == Constants.DriveConstants.GYRO_TYPE_NAVX) {
+	    SmartDashboard.putData("Gyro", navXGyro);
+	}
+	else {
+	    SmartDashboard.putData("Gyro", adxrs450Gyro);
+	}
+	SmartDashboard.putNumber("Heading", getHeading());
     }
 }
