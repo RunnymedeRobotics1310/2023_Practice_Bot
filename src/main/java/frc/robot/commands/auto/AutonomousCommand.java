9814d41bb75ebf10107a75e792e56a8107edefd8
Richard McMullin <richard.mcmullin@altairix.com>
2023-01-17 20:30:20 -0500

    Eric-Faber connit 11cf6a6

--- src/main/java/frc/robot/commands/auto/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/auto/AutonomousCommand.java
@@ -24,48 +24,62 @@ public class AutonomousCommand extends SequentialCommandGroup {
         switch (selectedAuto) {
 
         case AutoConstants.AUTO_PATTERN_SCORE_LOW:
-            //  Bottom score mid depart
+            // Bottom score mid depart
             System.out.println("Score Low auto pattern selected");
-            // TODO: 
+            // TODO:
             /*
-            move arm low
-            Open pads
-            Close pads
-            */
+             * move arm low
+             * Open pads
+             * Close pads
+             */
 
-            case AutoConstants.AUTO_PATTERN_SCORE_MID:
-            //  Bottom score mid depart
+        case AutoConstants.AUTO_PATTERN_SCORE_MID:
+            // Bottom score mid depart
             System.out.println("Score mid auto pattern selected");
-            // TODO: 
+            // TODO:
             /*
-            move arm mid
-            Open pads
-            Close pads
-            */
+             * move arm mid
+             * Open pads
+             * Close pads
+             */
             addCommands(new InstantCommand());
             break;
 
-            case AutoConstants.AUTO_PATTERN_REVERSE:
-            //  Bottom score mid depart
+        case AutoConstants.AUTO_PATTERN_REVERSE:
+            // Bottom score mid depart
             System.out.println("Score mid auto pattern selected");
-            // TODO: 
+            // TODO:
             /*
-            Drive backwards
-            */
+             * Drive backwards
+             */
             addCommands(new InstantCommand());
             break;
 
-            case AutoConstants.AUTO_PATTERN_GRAB_PIECE:
-            //  Bottom score mid depart
+        case AutoConstants.AUTO_PATTERN_GRAB_PIECE:
+            // Bottom score mid depart
             System.out.println("Score mid auto pattern selected");
-            // TODO: 
+            // TODO:
             /*
-            Open pads
-            Close pads
-            */
+             * Open pads
+             * Close pads
+             */
             addCommands(new InstantCommand());
             break;
 
+        case AutoConstants.AUTO_PATTERN_BALANCE:
+            // Balance on charge station
+            System.out.println("Balance auto pattern selected");
+            /*
+             * TODO
+             * balance
+             */
+            addCommands(new InstantCommand());
+            break;
+
+        default:
+            // How did we get here?
+            System.out.println("Auto selection(" + selectedAuto + ") was not programmed!");
+            addCommands(new InstantCommand());
         }
     }
 
