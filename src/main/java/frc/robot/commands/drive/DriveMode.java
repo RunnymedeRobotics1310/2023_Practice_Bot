024fadc7f4d0f5671bee331f24a1cc6a1a5399d2
q-field <quentin.field@student.tdsb.on.ca>
2023-01-19 17:59:27 -0500

    Created drive mode selector

---
+++ src/main/java/frc/robot/commands/drive/DriveMode.java
@@ -0,0 +1,5 @@
+package frc.robot.commands.drive;
+
+public enum DriveMode {
+    TANK, ARCADE, QUENTIN;
+}
---
+++ src/main/java/frc/robot/commands/drive/DriveModeSelector.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class DriveModeSelector {
+
+    public SendableChooser<DriveMode> driveModeChooser;
+    {
+        driveModeChooser = new SendableChooser<DriveMode>();
+        driveModeChooser.setDefaultOption("Arcade", DriveMode.ARCADE);
+        driveModeChooser.addOption("Arcade", DriveMode.ARCADE);
+        driveModeChooser.addOption("Quentin", DriveMode.QUENTIN);
+
+        SmartDashboard.putData("Drive Mode", driveModeChooser);
+
+    }
+
+    public DriveMode getDriveMode() {
+        return driveModeChooser.getSelected();
+    }
+}
